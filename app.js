9639dc6630c09f8f556141fa8d5d792feeba11f1
Dacrol <jonas.ahrens@gmail.com>
2018-10-23 17:14:13 +0200

    Load routes from MongoDB

--- app.js
+++ app.js
@@ -19,98 +19,98 @@ const test = new ProxyModel({
   subdomain: 'apa.nodethat.net'
 })
 
-//const routesFromDB // = ... 
 
-// Read our routes
-let routes = {
-  "nodethat.net": 3000,
-  "www.nodethat.net": {
-    "redirect": "nodethat.net"
-  }
-}
+mongoose.connection.once('open', start);
 
-//Object.assign(routes, routesFromDB)
+async function start() {
 
-// const routes = require('./routing.json');
+  // Read our routes
+  const routesFromDB = await ProxyModel.find({})
 
-// läs in port från mongodb
+  let routes = require('./routing.json')
+  routesFromDB.forEach(route => {
+    Object.assign(routes, {[route.subdomain]: route.port})
+  })
+    
 
+  console.log(routes)
+  // const routes = require('./routing.json');
 
+  // Read all certs from certbot into an object
+  let certs = readCerts("/etc/letsencrypt/live");
 
-// Read all certs from certbot into an object
-let certs = readCerts("/etc/letsencrypt/live");
+  // Create a new reverse proxy
+  const proxy = httpProxy.createProxyServer();
 
-// Create a new reverse proxy
-const proxy = httpProxy.createProxyServer();
+  // Handle proxy errors - thus not breaking the whole
+  // reverse-proxy app if an app doesn't answer
+  proxy.on('error', function (e) {
+    console.log('Proxy error', Date.now(), e);
+  })
 
-// Handle proxy errors - thus not breaking the whole
-// reverse-proxy app if an app doesn't answer
-proxy.on('error',function(e){
-  console.log('Proxy error', Date.now(), e);
-})
+  // Create a new unencrypted webserver
+  // with the purpose to redirect all traffic to https
+  http.createServer((req, res) => {
 
-// Create a new unencrypted webserver
-// with the purpose to redirect all traffic to https
-http.createServer((req,res)=>{
+    let urlParts = req.url.split('/');
 
-  let urlParts = req.url.split('/');
+    // redirect to https
+    let url = 'https://' + req.headers.host + req.url;
+    res.writeHead(301, { 'Location': url });
+    res.end();
 
-  // redirect to https
-  let url = 'https://' + req.headers.host + req.url;
-  res.writeHead(301, {'Location': url});
-  res.end();
+  }).listen(80);
 
-}).listen(80);
+  // Create a new secure webserver
+  https.createServer({
+    key: certs['nodethat.net'].key,
+    cert: certs['nodethat.net'].cert
+  }, (req, res) => {
 
-// Create a new secure webserver
-https.createServer({
-  key: certs['nodethat.net'].key,
-  cert: certs['nodethat.net'].cert
-},(req,res) => {
+    // Set/replace response headers
+    setResponseHeaders(req, res);
 
-  // Set/replace response headers
-  setResponseHeaders(req,res);
-
-  // Routing
-  let host = req.headers.host,
+    // Routing
+    let host = req.headers.host,
       url = req.url,
       portToUse;
 
-  url += (url.substr(-1) != '/' ? '/' : '');
+    url += (url.substr(-1) != '/' ? '/' : '');
 
-  for(let route in routes){
-    let port = routes[route];
-    if(route.includes('/')){
-      route += (route.substr(-1) != '/' ? '/' : '')
+    for (let route in routes) {
+      let port = routes[route];
+      if (route.includes('/')) {
+        route += (route.substr(-1) != '/' ? '/' : '')
+      }
+      if (route == host) {
+        portToUse = port;
+      }
+      else if (url != '/' && (host + url).indexOf(route) == 0) {
+        portToUse = port;
+      }
     }
-    if(route == host){
-      portToUse = port;
+
+    // Redirects
+    if (portToUse && portToUse.redirect) {
+      let url = 'https://' + portToUse.redirect + req.url;
+      res.writeHead(301, { 'Location': url });
+      res.end();
     }
-    else if (url != '/' && (host + url).indexOf(route) == 0){
-      portToUse = port;
+
+    // Serve the correct app for a domain
+    else if (portToUse) {
+      proxy.web(req, res, { target: 'http://127.0.0.1:' + portToUse });
     }
-  }
+    else {
+      res.statusCode = 404;
+      res.end('No such url!');
+    }
 
-  // Redirects
-  if(portToUse && portToUse.redirect){
-    let url = 'https://' + portToUse.redirect + req.url;
-    res.writeHead(301, {'Location': url});
-    res.end();
-  }
+  }).listen(443);
 
-  // Serve the correct app for a domain
-  else if (portToUse){
-    proxy.web(req,res,{target:'http://127.0.0.1:' + portToUse});
-  }
-  else {
-    res.statusCode = 404;
-    res.end('No such url!');
-  }
+}
 
-}).listen(443);
-
-
-function setResponseHeaders(req,res){
+function setResponseHeaders(req, res) {
 
   // there is a built in node function called res.writeHead
   // that writes http response headers
@@ -118,10 +118,10 @@ function setResponseHeaders(req,res){
   res.oldWriteHead = res.writeHead;
 
   // and then replace it with our function
-  res.writeHead = function(statusCode, headers){
+  res.writeHead = function (statusCode, headers) {
 
     // set/replace our own headers
-    res.setHeader('x-powered-by','Love');
+    res.setHeader('x-powered-by', 'Love');
 
     // security related (turned off right now)
     // res.setHeader('strict-transport-security','max-age=31536000; includeSubDomains; preload');
@@ -131,22 +131,22 @@ function setResponseHeaders(req,res){
     // res.setHeader('content-security-policy',"default-src * 'unsafe-inline' 'unsafe-eval'");
 
     // call the original write head function as well
-    res.oldWriteHead(statusCode,headers);
+    res.oldWriteHead(statusCode, headers);
   }
 
 }
 
-function readCerts(pathToCerts){
+function readCerts(pathToCerts) {
 
   let certs = {},
-      domains = fs.readdirSync(pathToCerts);
+    domains = fs.readdirSync(pathToCerts);
 
   // Read all ssl certs into memory from file
-  for(let domain of domains){
+  for (let domain of domains) {
     let domainName = domain.split('-0')[0];
     certs[domainName] = {
-      key:  fs.readFileSync(path.join(pathToCerts,domain,'privkey.pem')),
-      cert: fs.readFileSync(path.join(pathToCerts,domain,'fullchain.pem'))
+      key: fs.readFileSync(path.join(pathToCerts, domain, 'privkey.pem')),
+      cert: fs.readFileSync(path.join(pathToCerts, domain, 'fullchain.pem'))
     };
     certs[domainName].secureContext = tls.createSecureContext(certs[domainName]);
   }
--- proxyModel.js
+++ proxyModel.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 const ProxyModel = mongoose.model('Proxy', new mongoose.Schema({
   port: Number,
   subdomain: String
-}));
+}))
 
 module.exports = ProxyModel
