3f050708616671a78e8aa3953b805fcbb51e5522
Dacrol <jonas.ahrens@gmail.com>
2018-12-14 15:45:39 +0100

    Add online & asleep fields to model

--- proxyModel.js
+++ proxyModel.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose')
 
 const ProxyModel = mongoose.model('Proxy', new mongoose.Schema({
-  port: Number,
-  subdomain: String
+  port: {
+    type: Number,
+    required: true
+  },
+  subdomain: {
+    type: String,
+    required: true
+  },
+  online: Boolean,
+  asleep: {
+    type: Boolean,
+    default: false
+  }
 }))
 
 module.exports = ProxyModel
